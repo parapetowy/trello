7154eb8afe317a307f81ba14080bad64e2bc0dd4
Hubert Hadyński <hubert.hadynski@gmail.com>
2018-07-23 17:29:35 +0200

    fixed js

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
       this.id = randomString();
       this.name = name;
-      this.element = generateTemplate('column-template', { name: this.name });
+      this.element = generateTemplate('column-template', { id: this.id, name: this.name });
       this.element.querySelector('.column').addEventListener('click', function (event) {
         if (event.target.classList.contains('btn-delete')) {
           self.removeColumn();
